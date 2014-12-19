139e346411e13b7c294e1606755053e08994c554
Skepter <skepter.misc@gmail.com>
2014-12-19 16:28:45 +0000

    Initial commit

---
+++ Plugin - SleepPotion/src/io/github/Skepter/SleepPotion/Main.java
@@ -0,0 +1,159 @@
+package io.github.Skepter.SleepPotion;
+
+import io.github.Skepter.SleepPotion.PacketBuilder.PacketType;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.entity.HumanEntity;
+import org.bukkit.entity.LivingEntity;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.PotionSplashEvent;
+import org.bukkit.event.inventory.PrepareItemCraftEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.ShapedRecipe;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.potion.Potion;
+
+public class Main extends JavaPlugin implements Listener {
+
+	/* Sets up the recipes and registers the events*/
+	@Override
+	public void onEnable() {
+		getServer().getPluginManager().registerEvents(this, this);
+
+		ShapedRecipe recipe1 = new ShapedRecipe(potionItemStack());
+		recipe1.shape(new String[] { "WWW", "WPW", "WWW" });
+		recipe1.setIngredient('W', Material.WOOL);
+		recipe1.setIngredient('P', Material.POTION);
+		getServer().addRecipe(recipe1);
+
+		ShapedRecipe recipe2 = new ShapedRecipe(potionExtendedItemStack());
+		recipe2.shape(new String[] { "WRW", "RPR", "WRW" });
+		recipe2.setIngredient('W', Material.WOOL);
+		recipe2.setIngredient('R', Material.REDSTONE);
+		recipe2.setIngredient('P', Material.POTION);
+		getServer().addRecipe(recipe2);
+	}
+
+	/* Wakes up all sleeping players in the event of a reload */
+	@Override
+	public void onDisable() {
+		for (Player player : Bukkit.getOnlinePlayers())
+			awakeFromBed(player);
+	}
+
+	/* Prevent players without permission from crafting */
+	@EventHandler
+	public void onCraft(PrepareItemCraftEvent event) {
+		for (HumanEntity entity : event.getViewers()) {
+			if (entity instanceof Player) {
+				Player player = (Player) entity;
+				if (event.getRecipe().getResult().isSimilar(potionItemStack())) {
+					if (!player.hasPermission("SleepPotion.potion") || !player.isOp()) {
+						event.getInventory().setItem(0, new ItemStack(Material.AIR));
+						return;
+					}
+				} else if (event.getRecipe().getResult().isSimilar(potionExtendedItemStack())) {
+					if (!player.hasPermission("SleepPotion.potionExtended") || !player.isOp()) {
+						event.getInventory().setItem(0, new ItemStack(Material.AIR));
+						return;
+					}
+				}
+			}
+		}
+	}
+
+	/* Puts players to sleep when the potion is thrown */
+	@EventHandler
+	public void onEvent(PotionSplashEvent event) {
+		ItemStack is = event.getPotion().getItem();
+		if (is.isSimilar(potionItemStack())) {
+			for (Player player : getSplashedEntities(event.getAffectedEntities())) {
+				event.setIntensity(player, 0.0D);
+				doBedAction(player, false);
+			}
+		} else if (is.isSimilar(potionExtendedItemStack())) {
+			for (Player player : getSplashedEntities(event.getAffectedEntities())) {
+				event.setIntensity(player, 0.0D);
+				doBedAction(player, true);
+			}
+		}
+	}
+
+	/* Makes a list of all of the players to affect */
+	private List<Player> getSplashedEntities(Collection<LivingEntity> entities) {
+		List<Player> players = new ArrayList<Player>();
+		for (LivingEntity entity : entities)
+			if (entity instanceof Player)
+				players.add((Player) entity);
+		return players;
+	}
+
+	/* The potion itemstack */
+	private ItemStack potionItemStack() {
+		ItemStack itemStack = new ItemStack(Material.POTION);
+		Potion potion = new Potion(46);
+		potion.setSplash(true);
+		potion.apply(itemStack);
+		ItemMeta meta = itemStack.getItemMeta();
+		meta.setDisplayName("Potion of Sleeping");
+		meta.setLore(Arrays.asList(new String[] { ChatColor.RED + "Sleepiness (0:05)" }));
+		itemStack.setItemMeta(meta);
+		itemStack.setDurability((short) 16384);
+		return itemStack;
+	}
+
+	/* The extended potion itemstack*/
+	private ItemStack potionExtendedItemStack() {
+		ItemStack itemStack = new ItemStack(Material.POTION);
+		Potion potion = new Potion(46);
+		potion.setSplash(true);
+		potion.apply(itemStack);
+		ItemMeta meta = itemStack.getItemMeta();
+		meta.setDisplayName("Potion of Sleeping");
+		meta.setLore(Arrays.asList(new String[] { ChatColor.RED + "Sleepiness (0:10)" }));
+		itemStack.setItemMeta(meta);
+		itemStack.setDurability((short) 16384);
+		return itemStack;
+	}
+
+	/* Puts the player to sleep, then wakes them up after a set time */
+	private void doBedAction(final Player player, boolean extended) {
+		putToBed(player);
+
+		int duration = 5;
+		if (extended)
+			duration = 10;
+
+		Bukkit.getScheduler().scheduleSyncDelayedTask(this, new Runnable() {
+
+			@Override
+			public void run() {
+				awakeFromBed(player);
+			}
+		}, duration * 20);
+	}
+
+	/* Puts the player to sleep 
+	 * a = the player to put to sleep
+	 * b,c,d = the location of the player */
+	private void putToBed(final Player player) {
+		new PacketBuilder(player, PacketType.PLAY_OUT_BED).set("a", player.getEntityId()).setLocation("b", "c", "d", player.getLocation()).send();
+	}
+
+	/* Wakes the player up from sleep 
+	 * a = the player to put to sleep
+	 * b = the type of animation (2 being the wake up from sleep animation) */
+	private void awakeFromBed(final Player player) {
+		new PacketBuilder(player, PacketType.PLAY_OUT_ANIMATION).set("a", player.getEntityId()).setInt("b", 2).send();
+	}
+}
